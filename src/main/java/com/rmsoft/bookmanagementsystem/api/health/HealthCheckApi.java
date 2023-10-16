7acf9bbde0090299bb19d57d571d8d9e9b185643
장병순 <byeongsoon94@gmail.com>
2023-10-16 15:53:23 +0900

    프로젝트 기초설정 (#4)
    * setting : application.yml 파일 암호화하여 작성
    
    * feat : Health Check API 구현

---
+++ src/main/java/com/rmsoft/bookmanagementsystem/api/health/HealthCheckApi.java
@@ -0,0 +1,17 @@
+package com.rmsoft.bookmanagementsystem.api.health;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/healthCheck")
+public class HealthCheckApi {
+
+    @GetMapping
+    public ResponseEntity<String> healthCheck() {
+        return ResponseEntity.ok("status : UP");
+    }
+
+}
