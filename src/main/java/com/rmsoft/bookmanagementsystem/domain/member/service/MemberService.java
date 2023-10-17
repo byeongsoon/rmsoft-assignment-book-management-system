a24258a4d065618f5421c3f1cf0692bd03d48206
byeongsoon <byeongsoon94@gmail.com>
2023-10-17 15:03:06 +0900

    feat : 전역에러 핸들링 추가하여 동일한 이름, 유저ID 중복체크

--- src/main/java/com/rmsoft/bookmanagementsystem/domain/member/service/MemberService.java
+++ src/main/java/com/rmsoft/bookmanagementsystem/domain/member/service/MemberService.java
@@ -2,6 +2,7 @@ package com.rmsoft.bookmanagementsystem.domain.member.service;
 
 import com.rmsoft.bookmanagementsystem.domain.member.model.Member;
 import com.rmsoft.bookmanagementsystem.domain.member.repository.MemberRepository;
+import com.rmsoft.bookmanagementsystem.global.error.exception.BusinessException;
 
 import lombok.RequiredArgsConstructor;
 
@@ -17,9 +18,11 @@ public class MemberService {
 
     public Member save(final Member newMember) {
         Optional<Member> findMember = memberRepository.findByNameAndUserId(newMember.getName(), newMember.getUserId());
+
         if (findMember.isPresent()) {
-            throw new IllegalArgumentException("이미 존재하는 회원입니다.");
+            throw new BusinessException(409, "이미 존재하는 회원입니다.");
         }
+
         return memberRepository.save(newMember);
     }
 
